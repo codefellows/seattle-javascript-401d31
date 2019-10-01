26c6996a33ae725944b5b8296c14913bb654e667
JacobKnaack <jacobknck@gmail.com>
2019-10-01 12:59:04 -0700

    adds typed-demo from lecture

---
+++ curriculum/class-37/typed-demo/react-auth/src/app.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import Auth from './components/auth/auth';
+import Login from './components/auth/login';
+import LoginProvider from './components/auth/context';
+
+const Read = props => {
+  return (
+    <Auth capability="read">
+      <span>Read</span>
+    </Auth>
+  );
+};
+
+const Update = props => {
+  return (
+    <Auth capability="update">
+      <span>Update</span>
+    </Auth>
+  );
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <LoginProvider>
+        <Login />
+        <hr />
+        <Read />
+        <Update />
+      </LoginProvider>
+    );
+  }
+}
+
+export default App;
---
+++ curriculum/class-37/typed-demo/react-auth/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app';
+
+const Main = () => {
+  return (
+    <App />
+  );
+};
+
+const root = document.getElementById('root');
+ReactDOM.render(<Main />, root);
